387565e22d55c356b86765978db90a3b1a2a7c2b
EnricoMariaFlati <enricomariaflati@gmail.com>
2022-06-27 18:50:00 +0200

    Aggiunta modifica 2

--- src/Attori/Amministratore.py
+++ src/Attori/Amministratore.py
@@ -3,7 +3,6 @@ from Attori.Utente import Utente
 
 class Amministratore(Contattabile, Utente):
 
-    
     def __init__(self, email : str, cellulare : str, username : str, password : str):
         credenziali = {'username': username, 'password': password}
         super().__init__(email, cellulare, **credenziali)
