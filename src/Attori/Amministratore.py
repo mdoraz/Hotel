6baa3981faef8020f1dc0b15ee37b84f2ef16ee4
enricomariaflati <enricomariaflati@gmail.com>
2022-06-27 11:47:40 +0200

    Aggiunta modifica

--- src/Attori/Amministratore.py
+++ src/Attori/Amministratore.py
@@ -2,6 +2,7 @@ from Attori.Contattabile import Contattabile
 from Attori.Utente import Utente
 
 class Amministratore(Contattabile, Utente):
+
     
     def __init__(self, email : str, cellulare : str, username : str, password : str):
         credenziali = {'username': username, 'password': password}
