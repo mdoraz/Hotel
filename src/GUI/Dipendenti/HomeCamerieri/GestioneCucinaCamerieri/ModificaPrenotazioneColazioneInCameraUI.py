859963f61386c109b69d728c3810f6596969dc75
EnricoMariaFlati <enricomariaflati@gmail.com>
2022-07-13 10:36:24 +0200

    Modifiche Struttura modifica colazione in camera + rispettivo file.ui

--- src/GUI/Dipendenti/HomeCamerieri/GestioneCucinaCamerieri/ModificaPrenotazioneColazioneInCameraUI.py
+++ src/GUI/Dipendenti/HomeCamerieri/GestioneCucinaCamerieri/ModificaPrenotazioneColazioneInCameraUI.py
@@ -9,17 +9,35 @@ class ModificaPrenotazioneColazioneInCameraUI(QTabWidget):
     def __init__(self, previous: QWidget):
         super().__init__()
         loadUi(GestoreFile.absolutePath('ModificaPrenotazioneColazioneInCamera.ui',Path.cwd()), self)
-        self.setMinimumSize(600, 600)
+        self.setMinimumSize(400, 200)
         self.setFont(QtGui.QFont('Arial', 10))
         self._connectButtons()
         self.previous = previous
 
+        self._hideWidget()
+
+    def _hideWidget(self):
+        self.widgetMenuModificare.hide()
+        self.btnAnnulla.hide()
+
     def _connectButtons(self):
         self.btnConfermaModifiche.clicked.connect(self._btnConfermaModificheClicked)
+        self.btnCerca.clicked.connect(self._btnCercaClicked)
+        self.btnAnnulla.clicked.connect(self._btnAnnullaClicked)
         self.btnTornarePaginaPrecedente.clicked.connect(self._btnTornarePaginaPrecedenteClicked)
+
+    def _btnCercaClicked(self):
+        self.widgetMenuModificare.show()
+        self.btnAnnulla.show()
+        self.setMinimumSize(600, 600)
+
+    def _btnAnnullaClicked(self):
+        self._hideWidget()
+        self.setMinimumSize(400, 200)
 
     def _btnConfermaModificheClicked(self):
         pass
+
 
     def _btnTornarePaginaPrecedenteClicked(self):
         self.close()
